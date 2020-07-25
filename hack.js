e2d100125d5acd1569e2932e2db2012a140f5609
pantnt <59225083+pantnt@users.noreply.github.com>
2020-07-25 10:26:27 +0800

    Add files via upload

--- hack.js
+++ hack.js
@@ -1,12 +1,13 @@
-Mod={
+Mods=[{
     Stroke:"#000000",
     Affect:function (){
         player.jumpspeed*=10
         console.log(player.jumpspeed)
     },
     generation:function (x,y,scale){
-        blocks.push([[x,y],[scale,25],"Mods"])
+        blocks.push([[x,y],[scale,25],"Tper"])
         
     },
-    gene:1/2,
-}
+    gene:1,
+    Name:"Tper",
+},]
