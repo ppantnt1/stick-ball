dbc0d54c57bdba13168f965d7d35f780dfc1aaf4
pantnt <59225083+pantnt@users.noreply.github.com>
2020-08-03 13:26:47 +0800

    Add files via upload

--- package/functions.js
+++ package/functions.js
@@ -1,7 +1,7 @@
 var cx;
 var cy;
 var cc=false;
-if(getDeviceType()=="desktop"){document.onmousemove=function(e) {
+if(getDeviceType()=="destop"){document.onmousemove=function(e) {
     var cc=false;
 	cx = event.clientX - cv.offsetLeft+400;
     cy = event.clientY - cv.offsetTop;
@@ -15,7 +15,86 @@ cv.addEventListener('mouseup',function(){
 })
 
 }else{
-    printnum(1,1,1)
+//are you watching?
+//if u are 
+//reply me at discord
+console.log(1)
+    cv.addEventListener("touchstart", handleStart, false);
+    cv.addEventListener("touchend", handleEnd, false);
+    cv.addEventListener("touchcancel", handleCancel, false);
+    cv.addEventListener("touchmove", handleMove, false);
+    function handleStart(evt) {
+        evt.preventDefault();
+        log("touchstart.");
+              
+        for (var i = 0; i < touches.length; i++) {
+          log("touchstart:" + i + "...");
+          ongoingTouches.push(copyTouch(touches[i]));
+          var color = colorForTouch(touches[i]);
+          ctx.beginPath();
+          ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
+          ctx.fillStyle = color;
+          ctx.fill();
+          log("touchstart:" + i + ".");
+        }
+      }    
+      function handleEnd(evt) {
+        evt.preventDefault();
+        log("touchend");
+        var touches = evt.changedTouches;
+      
+        for (var i = 0; i < touches.length; i++) {
+          var color = colorForTouch(touches[i]);
+          var idx = ongoingTouchIndexById(touches[i].identifier);
+      
+          if (idx >= 0) {
+            ctx.lineWidth = 4;
+            ctx.fillStyle = color;
+            ctx.beginPath();
+            ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+            ctx.lineTo(touches[i].pageX, touches[i].pageY);
+            ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
+            ongoingTouches.splice(idx, 1);  // remove it; we're done
+          } else {
+            log("can't figure out which touch to end");
+          }
+        }
+      }
+      function handleCancel(evt) {
+        evt.preventDefault();
+        log("touchcancel.");
+        var touches = evt.changedTouches;
+        
+        for (var i = 0; i < touches.length; i++) {
+          var idx = ongoingTouchIndexById(touches[i].identifier);
+          ongoingTouches.splice(idx, 1);  // remove it; we're done
+        }
+      }
+      function handleMove(evt) {
+        evt.preventDefault();
+        for (var i = 0; i < touches.length; i++) {
+          var color = colorForTouch(touches[i]);
+          var idx = ongoingTouchIndexById(touches[i].identifier);
+      
+          if (idx >= 0) {
+            log("continuing touch "+idx);
+            ctx.beginPath();
+            log("ctx.moveTo(" + ongoingTouches[idx].pageX + ", " + ongoingTouches[idx].pageY + ");");
+            ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+            log("ctx.lineTo(" + touches[i].pageX + ", " + touches[i].pageY + ");");
+            ctx.lineTo(touches[i].pageX, touches[i].pageY);
+            ctx.lineWidth = 4;
+            ctx.strokeStyle = color;
+            ctx.stroke();
+      
+            ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
+            log(".");
+          } else {
+            log("can't figure out which touch to continue");
+          }
+        }
+      }
+
 }
 function move(obj){
  obj.x+=obj.xs*unit/60+(obj.aoxs*5)/60
